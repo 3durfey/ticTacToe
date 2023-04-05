be86b0dc95c09986b86f5bdbacb652bf4bcb5498
pdurfey <3durfey@gmail.com>
2023-04-05 17:16:26 -0400

    added some functions for changing elements

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+const gameBtns = document.getElementsByClassName("gameBtn");
+
 //object for game board
 const gameboardObject = (() => {
-  let gameboard = [
-    ["1", "x", "x"],
-    ["x", "x", "x"],
-    ["x", "x", "9"],
-  ];
-  return { gameboard };
+  let gameboard = ["1", "x", "x", "x", "x", "x", "x", "x", "9"];
+  function addMove(x, userPiece) {
+    gameboard[x] = userPiece;
+    for (let x = 0; x < 9; x++) {
+      gameBtns[x].innerHTML = gameboardObject.gameboard[x];
+    }
+  }
+  return { gameboard, addMove };
 })();
 //facotry function to create players
 const playerFactory = (name) => {
-  return { name };
+  winner = false;
+  return { name, winner };
 };
 //display board
 const displayController = (() => {
   function display() {
-    for (let x = 0; x < 3; x++) {
-      for (let y = 0; y < 3; y++) {
-        console.log(gameboardObject.gameboard[x][y]);
-      }
+    for (let x = 0; x < 9; x++) {
+      gameBtns[x].innerHTML = gameboardObject.gameboard[x];
     }
   }
   return { display };
 })();
+
 displayController.display();
+
+function changeItem(buttonNum) {
+  gameBtns[buttonNum].innerHTML = "X";
+}
+for (let x = 0; x < 9; x++) {
+  gameBtns[x].addEventListener("click", function () {
+    gameboardObject.addMove(x, "O");
+  });
+}
