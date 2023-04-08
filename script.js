661fc62d81206776457803fe9f1da96416b6045e
pdurfey <3durfey@gmail.com>
2023-04-08 01:47:08 -0400

    cahnges

--- script.js
+++ script.js
@@ -17,12 +17,6 @@ player2 = playerFactory(
   localStorage.getItem("player2piece")
 );
 function setNames() {
-  console.log(
-    localStorage.getItem("player1name"),
-    localStorage.getItem("player1piece"),
-    localStorage.getItem("player2name"),
-    localStorage.getItem("player2piece")
-  );
   player1Display.innerHTML =
     localStorage.getItem("player1name") +
     "(" +
@@ -34,6 +28,7 @@ function setNames() {
     localStorage.getItem("player2piece") +
     ")";
   player1Display.style.color = "green";
+  game.AI = true;
 }
 //object for game board
 const gameboardObject = (() => {
@@ -45,12 +40,140 @@ const gameboardObject = (() => {
   }
   return { displayBoard, gameboard };
 })();
-
+//AI for move
+const AIobject = (() => {
+  let AIPiece = " ";
+  let userPiece = " ";
+  let userNum = 0;
+  function randomMove() {
+    for (let x = 0; x < 9; x++) {
+      if (gameboardObject.gameboard[x] === " ") {
+        game.addMove(x);
+        break;
+      }
+    }
+  }
+  function move(AI) {
+    //set piece values, x or o
+    AIPiece = AI;
+    AIPiece === "X" ? (userPiece = "O") : (userPiece = "X");
+    //first turn
+    if (game.turnNum === 1) {
+      if (gameboardObject.gameboard[0] === "X") {
+        game.addMove(4);
+        return;
+      } else if (gameboardObject.gameboard[2] === " ") {
+        game.addMove(2);
+        return;
+      } else if (gameboardObject.gameboard[6] === " ") {
+        game.addMove(6);
+        return;
+      } else if (gameboardObject.gameboard[8] === " ") {
+        game.addMove(8);
+        return;
+      }
+    }
+    userNum = 0;
+    for (let x = 0; x < 9; x++) {
+      if (gameboardObject.gameboard[x] === userPiece) {
+        userNum = userNum + x;
+      }
+    }
+    switch (userNum) {
+      case 1:
+        game.addMove(2);
+        break;
+      case 2:
+        game.addMove(1);
+        break;
+      case 3:
+        if (gameboardObject.gameboard[1] === userPiece) {
+          game.addMove(0);
+        } else {
+          game.addMove(6);
+        }
+        break;
+      case 4:
+        if (gameboardObject.gameboard[1] === userPiece) {
+          game.addMove(6);
+        } else {
+          game.addMove(8);
+        }
+        break;
+      case 5:
+        game.addMove(7);
+        break;
+      case 7:
+        if (gameboardObject.gameboard[2] === userPiece) {
+          game.addMove(8);
+        } else if (gameboardObject.gameboard[3] === userPiece) {
+          game.addMove(5);
+        } else {
+          game.addMove(3);
+        }
+        break;
+      case 6:
+        if (gameboardObject.gameboard[0] === userPiece) {
+          game.addMove(3);
+        } else {
+          game.addMove(6);
+        }
+        break;
+      case 8:
+        game.addMove(4);
+        break;
+      case 10:
+        if (gameboardObject.gameboard[2] === userPiece || game.turnNum > 5) {
+          game.addMove(5);
+        } else {
+          game.addMove(2);
+        }
+        break;
+      case 9:
+        if (gameboardObject.gameboard[2] === " ") {
+          game.addMove(0);
+        } else {
+          randomMove();
+        }
+        break;
+      case 11:
+        game.addMove(1);
+        break;
+      case 12:
+        game.addMove(0);
+        break;
+      case 13:
+        if (gameboardObject.gameboard[5] === userPiece) {
+          game.addMove(2);
+        } else {
+          game.addMove(8);
+        }
+        break;
+      case 14:
+        if (gameboardObject.gameboard[7] === " ") {
+          game.addMove(7);
+        } else {
+          randomMove();
+        }
+        break;
+      case 15:
+        if (gameboardObject.gameboard[6] === " ") {
+          game.addMove(6);
+        } else {
+          randomMove();
+        }
+        break;
+      default:
+        randomMove();
+    }
+  }
+  return { move };
+})();
 //object for game
 const game = (() => {
+  let turnNum = 0;
   let turn = player1;
   let winner = false;
-  function assignTurn() {}
   let AI = false;
   function checkForWinner() {
     const winners = [
@@ -78,7 +201,6 @@ const game = (() => {
   }
   function addMove(x) {
     if (gameboardObject.gameboard[x] === " " && winner === false) {
-      console.log(turn);
       gameboardObject.gameboard[x] = turn.playerPiece;
     }
     gameboardObject.displayBoard();
@@ -91,8 +213,12 @@ const game = (() => {
       player2Display.style.color = "green";
       player1Display.style.color = "black";
     }
+    this.turnNum++;
+    if ((AI = true && turn.name === "AI")) {
+      AIobject.move(turn.playerPiece);
+    }
   }
-  return { addMove, turn };
+  return { addMove, turn, AI, turnNum };
 })();
 
 //change turn on button click
@@ -106,7 +232,7 @@ function addEvents() {
     });
   }
 }
-//player one piece chosen
+//player one piece chosen-----------------------------------------------------------------
 let tempPlayer1;
 let tempPlayer2;
 function player1Chosen(playerPiece) {
@@ -130,9 +256,23 @@ function player1Chosen(playerPiece) {
     document.getElementById("getUserInput").innerHTML =
       "Enter Name for Player 2";
     document.getElementById(playerPiece).parentElement.remove();
+    document.getElementById("AI").style.visibility = "visible";
+    document.getElementById("AI").style.position = "relative";
     document.getElementById("name").value = "";
   }
 }
 function reset() {
   window.location.href = "index.html";
+}
+function vsAI() {
+  localStorage.setItem("player1name", tempPlayer1.name);
+  localStorage.setItem("player1piece", tempPlayer1.playerPiece);
+
+  localStorage.setItem("player2name", "AI");
+  if (tempPlayer1.playerPiece === "X") {
+    localStorage.setItem("player2piece", "O");
+  } else {
+    localStorage.setItem("player2piece", "X");
+  }
+  window.location.href = "board.html";
 }
--- test.js
+++ test.js
@@ -1,8 +1,40 @@
-//factory function to create players
-const playerFactory = (name) => {
-  winner = false;
-  return { name, winner };
-};
-const player1 = playerFactory("jon");
+012;
+345;
+678;
+let gameboard = [" ", "x", " ", "x", " ", "x", " ", " ", "x"];
+let userSpots = 0;
+let blank = true;
+let userPiece = "x";
+let AI = "AI";
+let limit = 6;
+let limit2 = 2;
+let directionRight = 1;
+let directionUpDown = 3;
+let x = 0;
+for (let y = 0; y <= limit; y += directionUpDown) {
+  userSpots = 0;
+  blank = -1;
 
-console.log();
+  for (x = 0; x <= limit2; x += directionRight) {
+    if (gameboard[y + x] === userPiece) {
+      userSpots++;
+    } else if (gameboard[y + x] === " ") {
+      blank = y + x;
+    }
+  }
+  if (userSpots > 1 && blank != -1) {
+    gameboard[blank] = AI;
+  } else {
+  }
+
+  if (y === 6 && limit === 6) {
+    directionRight = 3;
+    directionUpDown = 1;
+    limit = 3;
+    limit2 = 6;
+    y = -1;
+    x = -3;
+  }
+}
+
+console.log(gameboard);
